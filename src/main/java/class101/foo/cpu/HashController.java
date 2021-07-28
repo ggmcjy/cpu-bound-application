feec55b48b297d1de3b267eb331a4065eb388598
ggmcjy <ggmcjy94@naver.com>
2021-07-28 15:17:54 +0900

    change deploy4

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello deploy3";
+        return "hello deploy4";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
